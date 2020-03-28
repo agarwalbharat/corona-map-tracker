c33556821490bf49e1693d036a00bd0bb1a6a522
Bharat Agarwal <agarwalbharat68@gmail.com>
2020-03-28 15:40:43 +0530

    Detailed status of country

--- src/views/ViewCountry.vue
+++ src/views/ViewCountry.vue
@@ -1,13 +1,65 @@
 <template>
-  <h1>Hel</h1>
+  <v-content>
+    <v-container fluid>
+      <v-row justify="center" align="center">
+        <v-col cols="12" class="text-center"><p style="font-size:40px;" class="font-weight-bold">{{ co.country }}</p></v-col>
+        <v-col cols="12" class="text-center ma-1 mt-2 pa-0">
+          <p>Total Cases</p>
+          <p style="font-size:35px;" class="font-weight-bold">{{ co.cases }}</p>
+        </v-col>
+        <v-col cols="6" class="text-center">
+          <p>Deaths</p>
+          <p style="font-size:30px;" class="font-weight-bold red--text">{{ co.deaths }}</p>
+        </v-col>
+        <v-col cols="6" class="text-center">
+          <p>Recovered</p>
+          <p style="font-size:30px;" class="font-weight-bold green--text">{{ co.recovered }}</p>
+        </v-col>
+        <v-col cols="12" class="text-center">
+            <v-divider></v-divider>
+        </v-col>
+        <v-col cols="6" class="text-center">
+          <p>Today Cases</p>
+          <p style="font-size:30px;" class="font-weight-bold">{{ co.todayCases }}</p>
+        </v-col>
+        <v-col cols="6" class="text-center">
+          <p>Today Deaths</p>
+          <p style="font-size:30px;" class="font-weight-bold green--text">{{ co.todayDeaths }}</p>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-content>
 </template>
 
 <script>
 export default {
-
-}
+  name: "viewDetailed",
+  data: () => ({
+    isLoading: false,
+    co: {}
+  }),
+  created() {
+    this.getData();
+  },
+  methods: {
+    getData() {
+      this.isLoading = true;
+      fetch("https://corona.lmao.ninja/countries/" + this.$route.params.id, {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+        .then(res => res.json())
+        .then(doc => {
+          this.co = doc;
+        });
+    }
+  }
+};
 </script>
-
-<style>
-
+<style scoped>
+p {
+  margin: 0;
+  padding: 0;
+}
 </style>
