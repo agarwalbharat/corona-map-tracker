62be1bd9eb233c4a0256fb638233860196106685
Bharat Agarwal <agarwalbharat68@gmail.com>
2020-03-28 10:18:22 +0530

    vuex and router setuped

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,48 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-const routes = [
-  {
+const routes = [{
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'home',
+    component: () => import( /* webpackChunkName: "home" */ '@/views/Home.vue'),
+    meta: {
+      title: "Home - CoronaTracker",
+    }
   },
   {
     path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    name: 'about',
+    component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue'),
+    meta: {
+      title: "About - CoronaTracker",
+    }
+  },
+  {
+    path: '/map',
+    name: 'map',
+    component: () => import(/* webpackChunkName: "map" */ '@/views/Map.vue'),
+    meta: {
+      title: "Map - CoronaTracker",
+    }
+  },
+  {
+    path: '/country',
+    name: 'country',
+    component: () => import(/* webpackChunkName: "country" */ '@/views/Country.vue'),
+    meta: {
+      title: "Country - CoronaTracker",
+    }
+  },
+  {
+    path: '/country/:id',
+    name: 'country-detailed',
+    component: () => import(/* webpackChunkName: "ViewCountry.vue" */ '@/views/ViewCountry.vue.vue'),
+    meta: {
+      title: "Country - CoronaTracker",
+    }
+  },
 ]
 
 const router = new VueRouter({
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,55 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    items: [{
+        text: 'Home',
+        to: '/',
+        icon: 'mdi-home',
+        meta:{
+          showToolbar: true,
+          showBottomNav: true
+        }, 
+      },
+      {
+        text: 'Map',
+        to: '/map',
+        icon: 'mdi-google-maps',
+        meta:{
+          showToolbar: false,
+          showBottomNav: true
+        }, 
+      },
+      {
+        text: 'Country',
+        to: '/country',
+        icon: 'mdi-earth',
+        meta:{
+          showToolbar: true,
+          showBottomNav: true
+        }, 
+      },
+      {
+        text: 'About',
+        to: '/about',
+        icon: 'mdi-account-multiple',
+        meta:{
+          showToolbar: false,
+          showBottomNav: true
+        }, 
+      },
+    ]
+  },
+  getters: {
+    links:(state)=>state.links,
+    bottomLink: (state)=> state.links.filter(link => link.meta.showBottomNav === true),
+    appBarLinks: (state)=> state.links.filter(link => link.meta.showAppBar === true),
   },
   mutations: {
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {}
+});
