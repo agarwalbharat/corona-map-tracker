c9bb36318687092f3c63c5b2c56023695cc16833
Bharat Agarwal <agarwalbharat68@gmail.com>
2020-03-28 15:50:56 +0530

    About section

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-content class="ma-1">
+    <v-row justify="center" align="center">
+      <v-col>
+        <v-img
+          :src="require('@/assets/about.svg')"
+          :lazy-src="require('@/assets/about.svg')"
+          max-height="150"
+          contain
+          class="transparent mb-5"
+        >
+          <template v-slot:placeholder>
+            <v-row class="fill-height ma-0" align="center" justify="center">
+              <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+            </v-row>
+          </template>
+        </v-img>
+        <p class="text-center">
+          A Project Made with
+          <v-icon>mdi-heart</v-icon> by
+          <a href="https://iambharat.tk" target="_blank" style="text-decoration:none">Bharat Agarwal</a>
+        </p>
+      </v-col>
+    </v-row>
+  </v-content>
 </template>
